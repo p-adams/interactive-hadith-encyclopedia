6e3d1faabc68c609c19cf4e75a9f2c3c523bf5a4
Paul Adams <ghusiyy@gmail.com>
2022-09-29 00:08:02 -0700

    render bios

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import "./App.css";
+import React, { useEffect, useState } from "react";
 
+type Biography = {
+  id: string;
+  data: string;
+};
+
+import "./App.css";
+const ENDPOINT = import.meta.env.VITE_API_URL;
 function App() {
+  const [bios, setBios] = useState<Biography[]>([]);
+  useEffect(() => {
+    fetch(ENDPOINT)
+      .then((r) => r.json())
+      .then((res: Biography[]) => setBios(res));
+  }, [bios]);
   return (
     <div className="App">
       <header>
         <h2>Interactive Hadith Encyclopedia</h2>
       </header>
-      <main>content</main>
+      <main>
+        <div className="biographies-table">
+          <div className="cell">id</div>
+          <div className="cell">data</div>
+          {bios.map((bio, idx) => (
+            <React.Fragment key={idx}>
+              <div className="cell">{bio.id}</div>
+              <div className="cell">{bio.data}</div>
+            </React.Fragment>
+          ))}
+        </div>
+      </main>
       <footer>footer</footer>
     </div>
   );
